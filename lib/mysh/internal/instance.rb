948f0a845a03ce9b6af8c7051551a98e8bb983b4
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-07-21 22:26:04 -0400

    Refactored the InternalCommand class.

---
+++ lib/mysh/internal/instance.rb
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+#* internal.rb -- mysh internal command instance data and methods.
+module Mysh
+
+  #The mysh internal command instance data and methods.
+  class InternalCommand
+    #The name of the command.
+    attr_reader :name
+
+    #The description of the command.
+    attr_reader :description
+
+    #The action of the command.
+    attr_reader :action
+
+    #Setup an internal command
+    def initialize(name, description, &action)
+      @name        = name
+      @description = description
+      @action      = action
+    end
+
+    #Execute the command.
+    def execute(args)
+      @action.call(args)
+    end
+
+    #Get information about the command.
+    def info
+      [@name, @description]
+    end
+
+  end
+
+end
---
+++ lib/mysh/internal/klass.rb
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+#* internal/klass.rb -- mysh internal command class level data and methods.
+module Mysh
+
+  #The mysh internal command class level data and methods.
+  class InternalCommand
+
+    #Set up the command library hash.
+    @commands = {}
+
+    class << self
+      #The command library, a hash.
+      attr_reader :commands
+    end
+
+    #Add a command to the command library.
+    def self.add(command)
+      @commands[command.name] = command
+    end
+
+    def self.add_alias(new_name, old_name)
+      unless (command = @commands[old_name])
+        fail "Error adding alias #{new_name} for #{old_name}"
+      end
+
+      @commands[new_name] = new(new_name,
+                                command.description,
+                                &command.action)
+    end
+
+    #Execute an internal command
+    def self.execute(str)
+      args = str.split
+
+      if (command = @commands[args[0]])
+        command.execute(args[1..-1])
+        true
+      else
+        false
+      end
+    end
+
+  end
+end
+
