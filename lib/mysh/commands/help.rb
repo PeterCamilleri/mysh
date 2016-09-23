fdd3eebc8f4a505d43ce02cc910158d60770a7db
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-09-23 19:42:53 -0400

    Further refactored help display.

--- lib/mysh/commands/help.rb
+++ lib/mysh/commands/help.rb
@@ -12,7 +12,7 @@ module Mysh
 
       if args.empty?
         puts IO.read(File.dirname(__FILE__) + '/help_internal.txt')
-        InternalCommand.display_internal_help_info
+        InternalCommand.display_info(info)
         puts IO.read(File.dirname(__FILE__) + '/help.txt')
       elsif args[0] == 'math'
         puts IO.read(File.dirname(__FILE__) + '/help_math.txt')
@@ -27,7 +27,7 @@ module Mysh
     add_alias('?', 'help')
 
     #Display internal help information.
-    def self.display_internal_help_info
+    def self.display_info(info)
       #Get the help info sorted.
       help_info = info.sort {|first, second | first[0] <=> second[0] }
 
@@ -35,13 +35,13 @@ module Mysh
       tag_width = help_info.max_by {|item| item[0].length}[0].length + 1
 
       #Display the information.
-      help_info.each {|item| display_help_item(item, tag_width) }
+      help_info.each {|item| display_item(item, tag_width) }
 
       puts
     end
 
     #Display one item of help.
-    def self.display_help_item(item, tag_width)
+    def self.display_item(item, tag_width)
       tag = item[0]
 
       item[1].each do |detail|
