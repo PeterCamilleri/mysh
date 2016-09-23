6b8a2e3bc1d9b4c0939842178480f81a0a6362ba
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-09-23 19:50:42 -0400

    More display refactoring.

--- lib/mysh/commands/help.rb
+++ lib/mysh/commands/help.rb
@@ -12,7 +12,7 @@ module Mysh
 
       if args.empty?
         puts IO.read(File.dirname(__FILE__) + '/help_internal.txt')
-        InternalCommand.display_info(info)
+        InternalCommand.display_items(info)
         puts IO.read(File.dirname(__FILE__) + '/help.txt')
       elsif args[0] == 'math'
         puts IO.read(File.dirname(__FILE__) + '/help_math.txt')
@@ -25,30 +25,6 @@ module Mysh
     end
 
     add_alias('?', 'help')
-
-    #Display internal help information.
-    def self.display_info(info)
-      #Get the help info sorted.
-      help_info = info.sort {|first, second | first[0] <=> second[0] }
-
-      #Determine the width of the tag area.
-      tag_width = help_info.max_by {|item| item[0].length}[0].length + 1
-
-      #Display the information.
-      help_info.each {|item| display_item(item, tag_width) }
-
-      puts
-    end
-
-    #Display one item of help.
-    def self.display_item(item, tag_width)
-      tag = item[0]
-
-      item[1].each do |detail|
-        puts "#{tag.ljust(tag_width)} #{detail}"
-        tag = ""
-      end
-    end
 
   end
 
