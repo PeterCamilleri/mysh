2598021804b5017b766b7597459920584aa7fc6a
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-09-29 07:40:05 -0400

    Unsaved file.

--- lib/mysh/support/handlebars.rb
+++ lib/mysh/support/handlebars.rb
@@ -1,13 +1,13 @@
 # coding: utf-8
 
-#* support/text_erb.rb -- Allow text files to embed ruby code.
+#* support/handlebars.rb -- Allow text files to embed ruby code.
 module Mysh
 
-  #The mysh embedded ruby report formatting.
+  #The mysh embedded ruby text formatting.
   class InternalCommand
 
     #Process a string with embedded Ruby code.
-    def process_erb_string(str)
+    def expand_handlebars(str)
       loop do
         pre_match, match, post_match = str.partition(/{{.*?}}/m)
 
@@ -20,7 +20,7 @@ module Mysh
 
     #Show a file with embedded ruby handlebars.
     def show_file(name)
-      puts process_erb_string(IO.read(full_name = COMMAND_PATH + name))
+      puts expand_handlebars(IO.read(full_name = COMMAND_PATH + name))
     rescue StandardError, ScriptError => err
       puts "Error in file: #{full_name}\n#{err.class}: #{err}"
     end
