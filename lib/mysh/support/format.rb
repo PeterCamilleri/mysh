f98c3415021aaf452e098fe30b3262a75dd5d841
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-03 18:31:49 -0400

    Formatting methods are now provate methods of Object, just like puts etc.

--- lib/mysh/support/format.rb
+++ lib/mysh/support/format.rb
@@ -1,50 +1,48 @@
 # coding: utf-8
 
-#* support/format.rb -- Format mysh internal reports.
-module Mysh
+#The mysh internal command class level report formatting.
+class Object
 
-  #The mysh internal command class level report formatting.
-  class Command
+  private
 
-    #Display an array of items.
-    def display_items(items)
-      puts format_items(items)
-      puts
-    end
-
-    #Format an array of items.
-    #<br>Endemic Code Smells
-    #* :reek:FeatureEnvy
-    def format_items(items, buffer=[])
-      #Determine the width of the tag area.
-      tag_width = items.max_by {|item| item[0].length}[0].length + 1
-
-      #Display the information.
-      items.each {|item| format_item(item, buffer, tag_width) }
-
-      buffer
-    end
-
-    #Display one item.
-    def display_item(item, tag_width=nil)
-      puts format_item(item, [], tag_width)
-      puts
-    end
-
-    #Format one item.
-    #<br>Endemic Code Smells
-    #* :reek:UtilityFunction
-    def format_item(item, buffer=[], tag_width)
-      tag = item[0]
-
-      item[1].each do |detail|
-        buffer << "#{tag.ljust(tag_width)} #{detail}"
-        tag = ""
-      end
-
-      buffer
-    end
-
+  #Display an array of items.
+  def display_items(items)
+    puts format_items(items)
+    puts
   end
+
+  #Format an array of items.
+  #<br>Endemic Code Smells
+  #* :reek:FeatureEnvy
+  def format_items(items, buffer=[])
+    #Determine the width of the tag area.
+    tag_width = items.max_by {|item| item[0].length}[0].length + 1
+
+    #Display the information.
+    items.each {|item| format_item(item, buffer, tag_width) }
+
+    buffer
+  end
+
+  #Display one item.
+  def display_item(item, tag_width=nil)
+    puts format_item(item, [], tag_width)
+    puts
+  end
+
+  #Format one item.
+  #<br>Endemic Code Smells
+  #* :reek:UtilityFunction
+  def format_item(item, buffer=[], tag_width)
+    tag = item[0]
+
+    item[1].each do |detail|
+      buffer << "#{tag.ljust(tag_width)} #{detail}"
+      tag = ""
+    end
+
+    buffer
+  end
+
 end
 
