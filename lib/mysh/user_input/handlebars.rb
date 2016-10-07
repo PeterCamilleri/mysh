d0a809880132c908308b538b57a3315c92d7cc4e
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-07 12:00:47 -0400

    Clarified the role of the message recvr in handlebar methods.

--- lib/mysh/user_input/handlebars.rb
+++ lib/mysh/user_input/handlebars.rb
@@ -4,6 +4,8 @@
 class Object
 
   #Show a file with embedded ruby handlebars.
+  #<br>Note:
+  #The message receiver is the evaluation host for the handlebar code.
   def show_handlebar_file(name)
     puts eval_handlebar_file(name)
   rescue Interrupt, StandardError, ScriptError => err
@@ -11,11 +13,15 @@ class Object
   end
 
   #Expand a file with embedded ruby handlebars.
+  #<br>Note:
+  #The message receiver is the evaluation host for the handlebar code.
   def eval_handlebar_file(name)
     eval_handlebars(IO.read(name))
   end
 
   #Process a string with backslash quotes and code embedded in handlebars.
+  #<br>Note:
+  #The message receiver is the evaluation host for the handlebar code.
   def eval_handlebars(str)
     do_process_handlebars(str).gsub(/\\\S/) {|found| found[1]}
   end
@@ -23,6 +29,8 @@ class Object
   private
 
   #Process a string with code embedded in handlebars.
+  #<br>Note:
+  #The message receiver is the evaluation host for the handlebar code.
   def do_process_handlebars(str)
     str.gsub(/{{.*?}}/m) do |match|
       code   = match[2...-2]
