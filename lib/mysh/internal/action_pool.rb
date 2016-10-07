f7dcab9ca4a1fe6d6ae9c1a19e3d154b70daa877
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-07 12:14:35 -0400

    Updated action_pool comments. Use action instead of command.

--- lib/mysh/internal/action_pool.rb
+++ lib/mysh/internal/action_pool.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
 
-#* internal/action_pool.rb -- A managed hash of mysh commands.
+#* internal/action_pool.rb -- A managed hash of mysh actions.
 module Mysh
 
-  #* A managed hash of mysh commands.
+  #* A managed hash of mysh actions.
   class ActionPool
 
-    #Create a new command pool
+    #Create a new action pool
     def initialize(&default_action)
       @pool = {}
 
@@ -15,31 +15,31 @@ module Mysh
       end
     end
 
-    #Get a command.
+    #Get a action.
     def [](index)
       @pool[index]
     end
 
-    #Add a command.
+    #Add a action.
     def add(name, description, &action)
       split_name = name.split[0] || ""
 
       @pool[split_name] = Action.new(name, description, &action)
     end
 
-    #Add an alias for an existing command.
+    #Add an alias for an existing action.
     def add_alias(new_name, old_name)
-      unless (command = @pool[old_name])
+      unless (action = @pool[old_name])
         fail "Error adding alias #{new_name} for #{old_name}"
       end
 
       split_name = new_name.split[0]
 
       @pool[split_name] =
-        Action.new(new_name, command.description, &command.action)
+        Action.new(new_name, action.description, &action.action)
     end
 
-    #Get information on all commands.
+    #Get information on all actions.
     def actions_info
       @pool
         .values
