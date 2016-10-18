b4ff7cb84afc1e7d443680e458338cdc9db917a9
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-17 21:02:00 -0400

    Removed silly code.

--- lib/mysh/internal/action_pool.rb
+++ lib/mysh/internal/action_pool.rb
@@ -12,7 +12,7 @@ module Mysh
     #Create a new action pool
     def initialize(pool_name, &default_action)
       @pool_name, @pool = pool_name, {}
-      @pool.default = default_action if default_action
+      @pool.default = default_action
     end
 
     #Get a action.
