1668625aa08c04aae469d059d0694e51761c6923
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-22 13:40:13 -0400

    Refactored the try_execute_quick_command method.

--- lib/mysh/quick.rb
+++ lib/mysh/quick.rb
@@ -3,29 +3,20 @@
 #* mysh/internal/quick.rb -- The mysh internal quick commands.
 module Mysh
 
+  #A hash of quick command short cuts and their actions.
+  QUICK = Hash.new(lambda {|_str| false})
+
+  QUICK['!'] = lambda {|str| HISTORY_COMMAND.quick_parse_and_call(str) }
+  QUICK['='] = lambda {|str| @exec_host.execute(str); :expression }
+  QUICK['?'] = lambda {|str| HELP_COMMAND.quick_parse_and_call(str) }
+  QUICK['@'] = lambda {|str| SHOW_COMMAND.quick_parse_and_call(str) }
+
   #Try to execute the string as a quick command.
   #<br>Endemic Code Smells
-  #* :reek:TooManyStatements
+  #* :re ek:TooManyStatements
   def self.try_execute_quick_command(str)
-    case str[0]
-    when '!'
-      HISTORY_COMMAND.quick_parse_and_call(str)
-
-    when '='
-      @exec_host.execute(str)
-      :expression
-
-    when '?'
-      HELP_COMMAND.quick_parse_and_call(str)
-
-    when '@'
-      SHOW_COMMAND.quick_parse_and_call(str)
-
-    else
-      false
-    end
+    QUICK[str[0]].call(str)
   end
-
 
 end
 
