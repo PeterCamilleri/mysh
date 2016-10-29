44798e08e064c6cb484fa48396c6985f0c72c028
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-29 11:10:14 -0400

    Fixed gem dependencies for version 0.2.4

--- lib/mysh/version.rb
+++ lib/mysh/version.rb
@@ -2,7 +2,7 @@
 
 module Mysh
   #The version string of MY SHell.
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 
   #A brief summary of this gem.
   SUMMARY = "mysh -- a Ruby inspired command line shell."
