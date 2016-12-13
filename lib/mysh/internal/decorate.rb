fd13e57c9fd5254d30697572b7d0635b1c7676e0
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-12-13 15:36:22 -0500

    Further refactoring for #16

--- lib/mysh/internal/decorate.rb
+++ lib/mysh/internal/decorate.rb
@@ -5,24 +5,18 @@ class String
 
   #Make the file name fit the local system.
   def decorate
-    dress_up_slashes.dress_up_quotes
+    self.dress_up_slashes
+        .dress_up_quotes
   end
 
   #Dress up slashes and backslashes.
   def dress_up_slashes
-    backslash? ? self.gsub("/", "\\") : self
+    MiniReadline::PLATFORM == :windows ? self.gsub("/", "\\") : self
   end
 
   #Dress up in quotes if needed.
   def dress_up_quotes
     self[' '] ? "\"#{self}\"" : self
-  end
-
-  #Does this file name use backslashes?
-  #<br>Endemic Code Smells
-  #* :reek:UtilityFunction
-  def backslash?
-    MiniReadline::PLATFORM == :windows
   end
 
 end
