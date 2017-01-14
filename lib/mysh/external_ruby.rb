9935aa567eb58ba7d845c07b250f9f94e2d46ccb
Peter Camilleri <peter.c.camilleri@gmail.com>
2017-01-13 19:08:52 -0500

    Added direct support for mysh scripts.

--- lib/mysh/external_ruby.rb
+++ lib/mysh/external_ruby.rb
@@ -5,15 +5,20 @@ module Mysh
 
   #Try to execute as a Ruby program.
   def self.try_execute_external_ruby(str)
-    cmd = str.split[0]
 
-    if cmd && File.extname(cmd) == '.rb'
-      new_command = "#{RbConfig.ruby} #{str}"
-      puts "=> #{new_command}"  if MNV[:debug]
-      system(new_command)
-      :ruby_exec
+    if (cmd = str.split[0])
+      ext = File.extname(cmd)
+
+      if ext == '.rb'
+        new_command = "#{RbConfig.ruby} #{str}"
+        puts "=> #{new_command}"  if MNV[:debug]
+        system(new_command)
+        :ruby_exec
+      elsif ext == '.mysh'
+        Mysh.process_file(cmd)
+        :mysh_script
+      end
     end
-
   end
 
 end
