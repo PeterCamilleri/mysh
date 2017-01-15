9d53e394e1cbbc1b8e853be47917d9d4aecce291
Peter Camilleri <peter.c.camilleri@gmail.com>
2017-01-15 10:19:01 -0500

    Added the load option.

---
+++ lib/mysh/internal/actions/command_line/load.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+#* mysh/internal/actions/load.rb -- The mysh load command.
+module Mysh
+
+  #* mysh/internal/actions/load.rb -- The mysh load command.
+  class LoadOption < CommandOption
+
+    #Skip over the argument for pre_boot.
+    def pre_boot(read_point)
+      get_arg(read_point)
+    end
+
+    #Execute the load command line option.
+    def post_boot(read_point)
+      mysh "load #{get_arg(read_point).decorate}"
+    end
+
+  end
+
+  #Add the load command line option to the library.
+  desc = 'Load the specified file into the mysh.'
+  COMMAND_LINE.add_action(LoadOption.new('--load filename', desc))
+  COMMAND_LINE.add_action(LoadOption.new('-l filename', desc))
+end
