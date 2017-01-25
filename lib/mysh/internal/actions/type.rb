72ceb52db8bac2b451f3f6fc45709252546b598a
Peter Camilleri <peter.c.camilleri@gmail.com>
2017-01-24 20:58:26 -0500

    Converted the type command to the simpler form.

--- lib/mysh/internal/actions/type.rb
+++ lib/mysh/internal/actions/type.rb
@@ -3,24 +3,19 @@
 #* mysh/internal/actions/type.rb -- The mysh type command.
 module Mysh
 
-  #* mysh/internal/actions/type.rb -- The mysh type command.
-  class TypeCommand < Action
-
-    #Execute the type command.
-    def process_command(args)
-      file_name = args.shift
-
-      if file_name
-        show_handlebar_file(file_name, BindingWrapper.new(binding))
-      else
-        fail "A text file must be specified."
-      end
-    end
-
-  end
-
   #Add the type command to the library.
   desc = 'Display a text file with support for optional embedded ' +
          'handlebars and mysh variables.'
-  COMMANDS.add_action(TypeCommand.new('type file', desc))
+
+  action = lambda do |args|
+    file_name = args.shift
+
+    if file_name
+      show_handlebar_file(file_name, BindingWrapper.new(binding))
+    else
+      fail "A text file must be specified."
+    end
+  end
+
+  COMMANDS.add_action(Action.new('type file', desc, &action))
 end
