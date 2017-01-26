0b121429b0e08f1aeb1bc3dd3b410a10c9fd4e9b
Peter Camilleri <peter.c.camilleri@gmail.com>
2017-01-26 11:40:05 -0500

    Fleshed out the input wrapper class.

--- lib/mysh/input_wrapper.rb
+++ lib/mysh/input_wrapper.rb
@@ -5,6 +5,41 @@ module Mysh
 
   #* mysh/internal/input_wrapper.rb -- An action compatible wrapper for a input.
   class InputWrapper
+
+    #Build an input wrapper.
+    def initialize(raw)
+      @raw = raw.chomp
+      @parsed = @cooked = nil
+    end
+
+    #Access the raw text.
+    attr_reader :raw
+
+    #Get the first raw character.
+    def nose
+      @raw[0] || ""
+    end
+
+    #Get the balance of the raw string.
+    def tail
+      @raw[1..-1] || ""
+    end
+
+    #Access the massaged text.
+    def cooked
+      @cooked ||= @raw.preprocess
+    end
+
+    #Get the command word if it exists.
+    def command
+      @raw.split[0] || ""
+    end
+
+    #Get the parsed command line.
+    def parsed
+      @parsed ||= parse_args(self.cooked)
+    end
+
   end
 
 end
