32c004c37eaefe3111d8821d15f6cb5a9b04cbed
Peter Camilleri <peter.c.camilleri@gmail.com>
2017-03-06 18:58:51 -0500

    Updated history help.

--- lib/mysh/internal/actions/history.rb
+++ lib/mysh/internal/actions/history.rb
@@ -56,10 +56,9 @@ module Mysh
   end
 
   #Add the history commands to the library.
-  desc = 'Display the mysh command history, or if an index is specified, ' +
-         'retrieve the command with that index value.'
-  COMMANDS.add_action(HistoryCommand.new('history <index>', desc))
+  desc = 'Display the mysh command history. See ?! for more info.'
+  COMMANDS.add_action(HistoryCommand.new('history <arg>', desc))
   #The history command action object.
-  HISTORY_COMMAND = HistoryCommand.new('!<index>', desc)
+  HISTORY_COMMAND = HistoryCommand.new('!<arg>', desc)
   COMMANDS.add_action(HISTORY_COMMAND)
 end
