1b642dcc76710447eec6b43a909d5413ac7a65c2
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-04 18:48:50 -0400

    Fixed the cd command for issue #76

--- lib/mysh/internal/actions/cd.rb
+++ lib/mysh/internal/actions/cd.rb
@@ -5,12 +5,14 @@ module Mysh
 
   #Add the cd command to the library.
   desc = 'Change directory to the optional <dir> parameter ' +
-         'and then display the current working directory.'
+         'or display the current working directory.'
 
   action = lambda do |input|
-    args = input.args
-    Dir.chdir(args[0]) unless args.empty?
-    puts Dir.pwd.to_host_spec
+    if (args = input.args).empty?
+      puts Dir.pwd.to_host_spec
+    else
+      Dir.chdir(args[0]) unless args.empty?
+    end
   end
 
   COMMANDS.add_action(Action.new('cd <dir>', desc, &action))
