5af7ae25c9f442d7a58665dcaa82295fd4225b98
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-02 13:54:07 -0500

    Renamed quit to exit.

--- lib/mysh/internal/actions/exit.rb
+++ lib/mysh/internal/actions/exit.rb
@@ -1,10 +1,10 @@
 # coding: utf-8
 
-#* mysh/internal/actions/quit.rb -- The mysh internal quit command.
+# The mysh internal exit command.
 module Mysh
 
-  #Add the quit command to the library.
-  desc = 'Quit out of the mysh.'
+  # Add the exit command to the library.
+  desc = 'Exit out of the mysh.'
 
-  COMMANDS.add_action(Action.new('quit', desc) {|_input| exit })
+  COMMANDS.add_action(Action.new('exit', desc) {|_input| exit })
 end
