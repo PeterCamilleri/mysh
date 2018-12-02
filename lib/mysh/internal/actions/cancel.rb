bfbf954b7e863b581cd76e72800d71ca7cb2ea6f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-02 16:18:49 -0500

    Reformatting

--- lib/mysh/internal/actions/cancel.rb
+++ lib/mysh/internal/actions/cancel.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 
-#The mysh internal cancel command.
+# The mysh internal cancel command.
 module Mysh
 
-  #Add the cancel command to the library.
+  # Add the cancel command to the library.
   desc = 'Cancel the current mysh level.'
 
   COMMANDS.add_action(Action.new('cancel', desc) {|_input| cancel})
