9bfa4c15b653e05a6f79b50a5777b9d049add289
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-05 00:58:02 -0500

    Added version info for @term

--- lib/mysh/internal/actions/show/term.rb
+++ lib/mysh/internal/actions/show/term.rb
@@ -21,7 +21,18 @@ module Mysh
     # Get the info
     # Endemic Code Smells :reek:UtilityFunction
     def info
-      [["code page", if MiniTerm.windows?; (`chcp`); end],
+      [["about",     MiniReadline::DESCRIPTION],
+       ["version",   MiniReadline::VERSION],
+       ["installed", Gem::Specification.find_all_by_name("mini_readline")
+                                       .map{|s| s.version.to_s}
+                                       .join(", ")],
+       ["about",     MiniTerm::DESCRIPTION],
+       ["version",   MiniTerm::VERSION],
+       ["installed", Gem::Specification.find_all_by_name("mini_term")
+                                       .map{|s| s.version.to_s}
+                                       .join(", ")],
+       ["latest",    Gem.latest_version_for("mini_term").to_s],
+       ["code page", if MiniTerm.windows?; (`chcp`); end],
        ["term",      ENV['TERM']],
        ["disp",      ENV['DISPLAY']],
        ["edit",      ENV['EDITOR']]
