e637e1c915d8e578807b4e66b40b08f3ff328ed9
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-05 10:35:16 -0500

    Updated @term.

--- lib/mysh/internal/actions/show/term.rb
+++ lib/mysh/internal/actions/show/term.rb
@@ -26,12 +26,27 @@ module Mysh
        ["installed", Gem::Specification.find_all_by_name("mini_readline")
                                        .map{|s| s.version.to_s}
                                        .join(", ")],
+       ["latest",    begin
+                       Gem.latest_version_for("mini_readline").to_s
+                     rescue => err
+                       err.to_s
+                     end
+       ],
        ["about",     MiniTerm::DESCRIPTION],
        ["version",   MiniTerm::VERSION],
        ["installed", Gem::Specification.find_all_by_name("mini_term")
                                        .map{|s| s.version.to_s}
                                        .join(", ")],
-       ["latest",    Gem.latest_version_for("mini_term").to_s],
+       ["latest",    begin
+                       Gem.latest_version_for("mini_term").to_s
+                     rescue => err
+                       err.to_s
+                     end
+       ],
+       ["platform",  MiniTerm::TERM_PLATFORM.inspect],
+       ["term type", MiniTerm::TERM_TYPE.inspect],
+       ["columns",   MiniTerm.width.to_s],
+       ["rows",      MiniTerm.height.to_s],
        ["code page", if MiniTerm.windows?; (`chcp`); end],
        ["term",      ENV['TERM']],
        ["disp",      ENV['DISPLAY']],
