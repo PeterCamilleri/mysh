4d07bd8a55b9abac0239bf32149aed7e57c5ebc7
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-28 10:30:20 -0500

    New for version 0.6.10

--- lib/mysh/version.rb
+++ lib/mysh/version.rb
@@ -2,7 +2,7 @@
 
 module Mysh
   #The version string of MY SHell.
-  VERSION = "0.6.9"
+  VERSION = "0.6.10"
 
   #A brief summary of this gem.
   SUMMARY = "mysh -- a Ruby inspired command line shell."
