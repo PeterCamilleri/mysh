c5151f0b485c6d3a680b4ef44f913ab3ddf1cfe1
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-01-27 13:48:35 -0500

    Refactor internal commands part one.

--- lib/mysh/internal.rb
+++ lib/mysh/internal.rb
@@ -3,7 +3,23 @@
 require_relative 'internal/actions/actions_path'
 require_relative 'internal/action'
 require_relative 'internal/action_pool'
-require_relative 'internal/manage'
 
-#Load up the internal actions!
+module Mysh
+
+  # Set up the command action pool.
+  COMMANDS = ActionPool.new("COMMANDS")
+
+  # Try to execute the string as an internal action.
+  def self.try_execute_internal(input)
+    unless input.quick_command == ' '
+      if (action = COMMANDS[input.raw_command])
+        action.process_command(input)
+        :internal
+      end
+    end
+  end
+
+end
+
+# Load up the internal actions!
 Dir[Mysh::Action::ACTIONS_PATH + '*.rb'].each {|file| require file }
