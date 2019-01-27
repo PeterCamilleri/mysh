92c76fb8dcbd0244a706c21f6af707c7af2a79b9
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-01-26 20:45:55 -0500

    Moved file name support to string helpers.

--- lib/mysh/string_helpers.rb
+++ lib/mysh/string_helpers.rb
@@ -12,4 +12,25 @@ class String
     end
   end
 
+  # Make the file name fit the local system.
+  def to_host_spec
+    self.dress_up_slashes
+        .dress_up_quotes
+  end
+
+  # Dress up slashes and backslashes.
+  def dress_up_slashes
+    MiniTerm.windows? ? self.gsub("/", "\\") : self
+  end
+
+  # Dress up in quotes if needed.
+  def dress_up_quotes
+    self[' '] ? "\"#{self}\"" : self
+  end
+
+  # Make the file name fit the standard notation.
+  def to_std_spec
+    self.gsub("\\", "/")
+  end
+
 end
