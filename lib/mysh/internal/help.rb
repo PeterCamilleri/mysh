d7b6e3434fcb2830bdf7479f8765c0c1ebac21aa
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-01-27 14:55:01 -0500

    Cleaned up help command.

--- lib/mysh/internal/help.rb
+++ lib/mysh/internal/help.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-#* mysh/internal/actions/help.rb -- The mysh internal help command.
+# The mysh internal help command.
 module Mysh
 
   # Help action pool of topics.
@@ -21,12 +21,11 @@ module Mysh
 
   HELP = ActionPool.new("HELP", default)
 
-  #* mysh/internal/actions/help.rb -- The mysh internal help command.
+  # The mysh internal help command.
   class HelpCommand < Action
 
-    #Execute a help command by routing it to a sub-command.
-    #<br>Endemic Code Smells
-    #* :reek:UtilityFunction
+    # Execute a help command by routing it to a sub-command.
+    # Endemic Code Smells  :reek:UtilityFunction
     def process_command(input)
       args = input.args
       HELP[args[0] || ""].process_command(args)
@@ -37,12 +36,11 @@ module Mysh
   # The base help command.
   desc = 'Display help information for mysh with an optional topic. See ?? for more.'
   COMMANDS.add_action(HelpCommand.new('help <topic>', desc))
+
   #The help command action object.
   HELP_COMMAND = HelpCommand.new('?<topic>', desc)
   COMMANDS.add_action(HELP_COMMAND)
 end
 
+# Load in support for help sub-topics
 require_relative 'help/sub_help'
-
-#Load up the extra help actions!
-Dir[Mysh::Action::ACTIONS_PATH + 'help/*.rb'].each {|file| require file }
