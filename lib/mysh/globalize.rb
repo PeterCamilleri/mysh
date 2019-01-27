60e3d21e89d97d1c9b730542fc0fbfd9145acae9
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-01-26 22:57:51 -0500

    Comment cleanup.

--- lib/mysh/globalize.rb
+++ lib/mysh/globalize.rb
@@ -1,16 +1,15 @@
 # coding: utf-8
 
-#Monkey patches for mysh global entities.
+# Monkey patches for mysh global entities.
 class Object
 
-  #Make the environment variable store accessible everywhere.
+  # Make the environment variable store accessible everywhere.
   MNV = Mysh::MNV
 
   private
 
-  #The mysh equivalent of the system method.
-  #<br>Endemic Code Smells
-  #* :reek:UtilityFunction
+  # The mysh equivalent of the system method.
+  # Endemic Code Smells  :reek:UtilityFunction
   def mysh(str)
     Mysh.try_execute_command(str)
   end
