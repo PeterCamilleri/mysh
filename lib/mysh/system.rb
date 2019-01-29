dc28b29669de643706fbc774a92a950f7481e937
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-01-29 10:12:50 -0500

    Comment clean up.

--- lib/mysh/system.rb
+++ lib/mysh/system.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 
-#* mysh/system.rb -- Support for executing through the system.
+# Support for executing through the system.
 module Mysh
 
-  #Try to execute as a system program.
+  # Try to execute as a system program.
   def self.try_execute_system(input)
     system(input.raw.preprocess.chomp + "\n") ? :system : :error
   end
