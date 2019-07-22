226fae336744c2e9a760be2bb1aaa5e15d1b8b16
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-07-21 20:05:18 -0400

    Fixes for issue #131 and version 0.6.14

--- lib/mysh/version.rb
+++ lib/mysh/version.rb
@@ -2,7 +2,7 @@
 
 module Mysh
   # The version string of MY SHell.
-  VERSION = "0.6.13".freeze
+  VERSION = "0.6.14".freeze
 
   # A brief summary of this gem.
   DESCRIPTION = "mysh -- a Ruby inspired command line shell.".freeze
